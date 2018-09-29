7b1905f3fbc821ae770a6bd4ae7aec2bfee0782a
charlidsun <sunjingge@outlook.com>
2018-09-29 17:19:25 +0800

    初始化项目

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Lychee
 微信公众号搜索工具
+
+# 前端 Element
---
+++ lychee/src/main/java/com/sun/lychee/LycheeApplication.java
@@ -0,0 +1,12 @@
+package com.sun.lychee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LycheeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LycheeApplication.class, args);
+	}
+}
